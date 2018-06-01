68992b83049463a52970d257b53e89c4c53092c8
cxqpfan <cxqpfan@163.com>
2018-06-02 07:20:03 +0800

    menu

--- src/components/AppComponent.vue
+++ src/components/AppComponent.vue
@@ -1,23 +1,7 @@
 <template>
     <div id="layout">
-        <!-- Menu toggle -->
-        <a href="#menu" id="menuLink" class="menu-link">
-            <!-- Hamburger icon -->
-            <span></span>
-        </a>
 
-        <div id="menu">
-            <div class="pure-menu" v-for="(item,index) in navigation" v-show="sideMenuIndex==index">
-                <a class="pure-menu-heading" href="#">{{item.label}}</a>
-                <ul class="pure-menu-list">
-                    <li class="pure-menu-item" v-for="page in item.pages">
-                        <router-link class="pure-menu-link" active-class="pure-menu-selected" :to="page.url">{{page.label}}</router-link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-        <div id="main">
+        <div>
             <div class="header">
                 <div class="pure-menu pure-menu-horizontal">
 
@@ -28,20 +12,25 @@
                     </ul>
                 </div>
             </div>
-            <div class="content">
-                <router-view></router-view>
-            </div>
         </div>
+
+        <SideMenu v-bind:navigation="navigation"></SideMenu>
+
+
+
     </div>
 </template>
 
 <script>
+    import SideMenu from './SideMenu.vue';
+
     export default {
-        data() {
-            return {
-                sideMenuIndex: 0
-            }
+        components: {
+            SideMenu
         },
-        props:['navigation']
+        data() {
+            return {}
+        },
+        props: ['navigation']
     }
 </script>
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div>
+        <div id="menu">
+            <div class="pure-menu" v-for="(item,index) in navigation" v-show="sideMenuIndex==index">
+                <a class="pure-menu-heading" href="#">{{item.label}}</a>
+                <ul class="pure-menu-list">
+                    <li class="pure-menu-item" v-for="page in item.pages">
+                        <router-link class="pure-menu-link" active-class="pure-menu-selected" :to="page.url">
+                            {{page.label}}
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div id="main">
+
+            <div class="content">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                sideMenuIndex: 0
+            }
+        },
+        props: ['navigation']
+    }
+</script>
